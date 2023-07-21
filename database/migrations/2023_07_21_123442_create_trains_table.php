f30b84c5e9cb6a01b86d670720487b05aeb9c77e
EmanuelaPau <uapame@gmail.com>
2023-07-21 14:50:11 +0200

    Add tables in migration

--- database/migrations/2023_07_21_123442_create_trains_table.php
+++ database/migrations/2023_07_21_123442_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,6 +15,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('company', 60);
+            $table->string('leaving_station', 60);
+            $table->string('arrival_station', 60);
+            $table->dateTime('leaving_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 20);
+            $table->tinyInteger('number_of_carts');
+            $table->boolean('in_time');
+            $table->boolean('canceled');
         });
     }
 
