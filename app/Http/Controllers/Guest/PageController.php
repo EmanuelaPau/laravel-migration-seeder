996d78e2cc68607a71062924ac9d16be4b9f9f26
EmanuelaPau <uapame@gmail.com>
2023-07-21 15:04:12 +0200

    Add controllers

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     //
+
+    public function home()
+    {
+        return view('guest.home');
+    }
 }
--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     //
+
+    public function index()
+    {
+        return view('guest.train.index');
+    }
 }
